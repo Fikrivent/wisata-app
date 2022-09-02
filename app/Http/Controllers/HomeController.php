46091475a5f541e302334735e0b774182a439622
Fikrivent <fikribaharuddin@gmail.com>
2022-09-02 19:18:09 +0700

    Database Integration and Dynamic UI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Wisata;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +24,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $wisatas = Wisata::all();
+        
+        $wst_favorit = Wisata::JOIN('pesan_tiket as p','wisata.idwisata','p.wisata_idwisata')
+        ->SelectRaw('count(wisata.idwisata')
+
+
+        return view('home', compact('wisatas'));
+
+    }
+
+    public function loadSelectBox($id)
+    {
+        $jadwals = Wisata::JOIN('jadwal_has_wisata as jw','wisata.idwisata','jw.wisata_idwisata')->JOIN('jadwal as j','jw.jadwal_idjadwal','j.idjadwal')
+        ->WHERE('wisata.idwisata','=',$id)->SELECT('j.idjadwal','j.hari','j.jam_awal', 'j.jam_akhir')->get();
+
+        //dd($jadwals);
+        return response()->json(['data' => $jadwals], 200);
     }
 }
